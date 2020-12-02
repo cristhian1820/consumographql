82222ae52f61eedb2854c05ff37cc2559b31e4bf
cristhian1820 <50779675+cristhian1820@users.noreply.github.com>
2020-12-02 12:54:55 -0500

    ejemplo de mutation para voike

--- consume/src/main/java/com/consume/graphql/consume/service/impl/PersonajeServiceImpl.java
+++ consume/src/main/java/com/consume/graphql/consume/service/impl/PersonajeServiceImpl.java
@@ -1,8 +1,9 @@
 package com.consume.graphql.consume.service.impl;
 
 import com.consume.graphql.consume.model.dto.personaje.InputPersonaje;
-import com.consume.graphql.consume.utils.RequestGraphql;
 import com.consume.graphql.consume.service.IPersonajeService;
+import com.consume.graphql.consume.utils.RandomString;
+import com.consume.graphql.consume.utils.RequestGraphql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class PersonajeServiceImpl implements IPersonajeService {
     @Override
     public ResponseEntity<String> obtenerPersonaje() {
 
-        String query = "{\n" +
+        String query = /*"{\n" +
                 "  allPersonajes{\n" +
                 "    id\n" +
                 "    nombre\n" +
@@ -32,13 +33,18 @@ public class PersonajeServiceImpl implements IPersonajeService {
                 "      nombre\n" +
                 "    }\n" +
                 "  }\n" +
-                "}";
+                "}";*/
 
-                //peticion para voika por json
-                //"{\"query\": \"{user(celular:\\\"+573168333879\\\"){id email}}\"}";
+                //Query para voika por json
+                "{\"query\": \"{" +
+                        " user(celular:\\\"+573168333879\\\"){" +
+                        " id" +
+                        " email " +
+                        " }" +
+                        " }" +
+                        "\"}";
 
         System.out.println(query);
-
 
 
         return requestGraphql.send(query);
@@ -46,6 +52,11 @@ public class PersonajeServiceImpl implements IPersonajeService {
 
     @Override
     public ResponseEntity<String> registrar(InputPersonaje input) {
+
+        /*String sbTransacionId = new RandomString().getAlphaNumericString(20);
+
+        System.out.println(">>" + sbTransacionId);*/
+
         String query = String.format("mutation{\n" +
                 "  registrarPersonaje(%s){    \n" +
                 "    id\n" +
@@ -55,7 +66,26 @@ public class PersonajeServiceImpl implements IPersonajeService {
                 "    fecha\n" +
                 "  }\n" +
                 "}\n",input.inputQuery());
-        System.out.println(query);
+
+//mutation para voike por json
+               /* "{\"query\":"
+                        + "\"mutation{ "
+                        + "   createTransaction(transaction:{"
+                        + "                                  user:\\\"665b7135-bffd-492a-9c38-42ebbd00c067\\\", "
+                        + "                                  buyed:2, "
+                        + "                                  billing:\\\"3333333\\\", "
+                        + "                                  currency:\\\"COL\\\", "
+                        + "                                  amount:2.5, "
+                        + "                                  status:\\\"A\\\", "
+                        + "                                  transaction_id:\\\"" + sbTransacionId + "\\\""
+                        + "                                 }"
+                        + "                    ){"
+                        + "                      id"
+                        + "                      user"
+                        + "                     }"
+                        + "          }\"" +
+                        "}";*/
+                System.out.println(query);
         return requestGraphql.send(query);
     }
 }
